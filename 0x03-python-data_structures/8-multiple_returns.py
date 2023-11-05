4a78f43d7011746164034ae264ffaf30c062e481
Simon <Simonziggy18@gmail.com>
2023-11-05 14:42:41 +0000

    mul

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,8 @@
 def multiple_returns(sentence):
     a = len(sentence)
     if a == 0:
-        return None
+        tuple_a = (a, None)
+        return tuple_a
     else:
         tuple_a = (a, sentence[0])
         return tuple_a
