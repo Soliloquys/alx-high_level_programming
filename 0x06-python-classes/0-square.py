dfb2cb5a1a34a3a82c5e13f6843ad0a07b849f79
Simon <Simonziggy18@gmail.com>
2023-11-23 01:55:56 +0000

    square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class represents a square
+
+    Attributes:
+        No attributes at the moment
+
+    Methods:
+        No methods at the moemnt
+    """
+
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        No methods at the moment.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
+        self.__size = size
