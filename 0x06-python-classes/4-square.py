2b551125b9329ee80c6372a4c5f3a22463df3dd3
Simon <Simonziggy18@gmail.com>
2023-11-25 21:08:10 +0000

    fin

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,11 +18,11 @@ class Square:
     @property
     def size(self):
         """Get the current size of the square"""
-        return(self.__size)
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not  isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
