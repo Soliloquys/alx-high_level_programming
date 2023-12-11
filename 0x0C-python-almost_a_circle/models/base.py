b3f8e847fa305660f2b4f15386b9df2942f6132c
Simon <Simonziggy18@gmail.com>
2023-12-11 21:23:20 +0000

    first

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 
+"""Defines a base model class"""
+
+
 class Base:
-    """ A base class for all other classes """
+    """Base Model.
+
+    A base class for all other classes
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Initializes an instance of the class """
+        """ Initializes an instance of the class
+
+        Args: id (int): the identity of the new instance
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
