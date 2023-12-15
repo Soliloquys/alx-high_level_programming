1865dc8eadeebb1ee7a36edad799cf2c6836bfe6
Simon <Simonziggy18@gmail.com>
2023-12-15 14:02:58 +0000

    rectangle

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines unittests for rectangle.py"""
+
+import unittest
+from models.base import Base
+from models.Rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """Unittests for testing of the Rectangle class"""
+    def test_
